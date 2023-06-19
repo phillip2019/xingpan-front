b5d875f29450be2096600d69c94cd14541a942f7
xiaowei.song <songxiaowei@chinagoods.com>
2023-06-19 16:13:31 +0800

    #Updates#添加各部门月KPI填报

---
+++ src/views/cg/CgDeptIndexValue1m.api.ts
@@ -0,0 +1,64 @@
+import {defHttp} from '/@/utils/http/axios';
+import { useMessage } from "/@/hooks/web/useMessage";
+
+const { createConfirm } = useMessage();
+
+enum Api {
+  list = '/cg/cgDeptIndexValue1m/list',
+  save='/cg/cgDeptIndexValue1m/add',
+  edit='/cg/cgDeptIndexValue1m/edit',
+  deleteOne = '/cg/cgDeptIndexValue1m/delete',
+  deleteBatch = '/cg/cgDeptIndexValue1m/deleteBatch',
+  importExcel = '/cg/cgDeptIndexValue1m/importExcel',
+  exportXls = '/cg/cgDeptIndexValue1m/exportXls',
+}
+/**
+ * 导出api
+ * @param params
+ */
+export const getExportUrl = Api.exportXls;
+/**
+ * 导入api
+ */
+export const getImportUrl = Api.importExcel;
+/**
+ * 列表接口
+ * @param params
+ */
+export const list = (params) =>
+  defHttp.get({url: Api.list, params});
+
+/**
+ * 删除单个
+ */
+export const deleteOne = (params,handleSuccess) => {
+  return defHttp.delete({url: Api.deleteOne, params}, {joinParamsToUrl: true}).then(() => {
+    handleSuccess();
+  });
+}
+/**
+ * 批量删除
+ * @param params
+ */
+export const batchDelete = (params, handleSuccess) => {
+  createConfirm({
+    iconType: 'warning',
+    title: '确认删除',
+    content: '是否删除选中数据',
+    okText: '确认',
+    cancelText: '取消',
+    onOk: () => {
+      return defHttp.delete({url: Api.deleteBatch, data: params}, {joinParamsToUrl: true}).then(() => {
+        handleSuccess();
+      });
+    }
+  });
+}
+/**
+ * 保存或者更新
+ * @param params
+ */
+export const saveOrUpdate = (params, isUpdate) => {
+  let url = isUpdate ? Api.edit : Api.save;
+  return defHttp.post({url: url, params});
+}
---
+++ src/views/cg/CgDeptIndexValue1m.data.ts
@@ -0,0 +1,253 @@
+import { BasicColumn } from '/@/components/Table';
+import { FormSchema } from '/@/components/Table';
+import { rules } from '/@/utils/helper/validator';
+import { render } from '/@/utils/common/renderUtils';
+//列表数据
+export const columns: BasicColumn[] = [
+  {
+    title: '指标所属部门',
+    align: 'center',
+    sorter: true,
+    dataIndex: 'deptText',
+  },
+  {
+    title: '指标名称',
+    align: 'center',
+    sorter: true,
+    dataIndex: 'indexNameZh',
+  },
+  {
+    title: '填报值',
+    align: 'center',
+    sorter: true,
+    dataIndex: 'indexValue',
+  },
+  {
+    title: '指标周期起始日期',
+    align: 'center',
+    sorter: true,
+    dataIndex: 'beginDate',
+    customRender: ({ text }) => {
+      return !text ? '' : text.length > 10 ? text.substr(0, 10) : text;
+    },
+  },
+  {
+    title: '指标周期结束日期',
+    align: 'center',
+    sorter: true,
+    dataIndex: 'endDate',
+    customRender: ({ text }) => {
+      return !text ? '' : text.length > 10 ? text.substr(0, 10) : text;
+    },
+  },
+  {
+    title: '备注',
+    align: 'center',
+    sorter: true,
+    dataIndex: 'remark',
+  },
+  {
+    title: '创建人',
+    align: 'center',
+    sorter: true,
+    dataIndex: 'createBy',
+  },
+  {
+    title: '创建时间',
+    align: 'center',
+    sorter: true,
+    dataIndex: 'createTime',
+  },
+];
+//查询数据
+export const searchFormSchema: FormSchema[] = [
+  {
+    label: '部门',
+    field: 'deptId',
+    component: 'JTreeDict',
+    helpMessage: ['请选择指标所属部门'],
+    componentProps: {
+      field: 'id',
+      async: true,
+      parentCode: 'B03',
+      placeholder: '请选择部门',
+    },
+    colProps: { span: 6 },
+  },
+  {
+    label: '指标名称',
+    field: 'deptIndexId',
+    component: 'JDictSelectTag',
+    helpMessage: ['请选择填报指标'],
+    componentProps: ({ schema, tableAction, formActionType, formModel }) => {
+      let sqlPreTpl = 'cg_dept_index,index_name_zh,id,1=1 ';
+      if (formModel.deptId) {
+        sqlPreTpl = sqlPreTpl + " and dept_id = '" + formModel.deptId + "'";
+      }
+      sqlPreTpl += ' and status = 1 order by create_time';
+      return {
+        dictCode: sqlPreTpl,
+      };
+    },
+    colProps: { span: 6 },
+  },
+  {
+    label: '指标值',
+    field: 'indexValue',
+    component: 'Input',
+    colProps: { span: 6 },
+  },
+  {
+    // TODO 添加默认动态值，填写上月月初
+    label: '开始日期',
+    field: 'beginDate',
+    component: 'DatePicker',
+    componentProps: {
+      valueFormat: 'YYYY-MM-DD',
+    },
+    colProps: { span: 6 },
+  },
+  {
+    // TODO 添加默认动态值，填写上月月末
+    label: '结束日期',
+    field: 'endDate',
+    component: 'DatePicker',
+    componentProps: {
+      valueFormat: 'YYYY-MM-DD',
+    },
+    colProps: { span: 6 },
+  },
+  {
+    label: '创建人',
+    field: 'createBy',
+    component: 'Input',
+    colProps: { span: 6 },
+  },
+  {
+    label: '创建时间',
+    field: 'createTime',
+    component: 'DatePicker',
+    componentProps: {
+      showTime: true,
+      valueFormat: 'YYYY-MM-DD HH:mm:ss',
+    },
+    colProps: { span: 6 },
+  },
+  {
+    label: '更新人',
+    field: 'updateBy',
+    component: 'Input',
+    colProps: { span: 6 },
+  },
+  {
+    label: '更新时间',
+    field: 'updateTime',
+    component: 'DatePicker',
+    componentProps: {
+      showTime: true,
+      valueFormat: 'YYYY-MM-DD HH:mm:ss',
+    },
+    colProps: { span: 6 },
+  },
+];
+//表单数据
+export const formSchema: FormSchema[] = [
+  {
+    label: '指标部门',
+    field: 'deptId',
+    component: 'JTreeDict',
+    helpMessage: ['请选择填报部门'],
+    // defaultValue: 'deptText',
+    componentProps: {
+      field: 'id',
+      async: true,
+      parentCode: 'B03',
+      placeholder: '请选择部门',
+      // dict: 'sys_category,name,id,1=1 order by create_time',
+      // pidField: 'pid',
+      // pidValue: '1625117453522718721',
+      // placeholder: '请选择部门',
+      // converIsLeafVal: 1,
+      // multiple: false,
+    },
+    dynamicRules: ({ model, schema }) => {
+      return [{ required: true, message: '请选择部门!' }];
+    },
+  },
+  {
+    label: '指标名称',
+    field: 'deptIndexId',
+    component: 'JDictSelectTag',
+    helpMessage: ['请选择填报指标'],
+    componentProps: ({ schema, tableAction, formActionType, formModel }) => {
+      // console.log(formActionType);
+      let sqlPreTpl = 'cg_dept_index,index_name_zh,id,1=1 ';
+      if (formModel.deptId) {
+        sqlPreTpl = sqlPreTpl + " and dept_id = '" + formModel.deptId + "'";
+      }
+      sqlPreTpl += ' and status = 1 and index_filling_method = 1  order by create_time';
+      return {
+        dictCode: sqlPreTpl,
+      };
+    },
+    dynamicRules: ({ model, schema }) => {
+      return [{ required: true, message: '请输入部门指标编号!' }];
+    },
+  },
+  {
+    label: '指标值',
+    field: 'indexValue',
+    component: 'InputNumber',
+    dynamicRules: ({ model, schema }) => {
+      return [
+        { required: true, message: '请输入部门值!' },
+        { pattern: /^-?\d+\.?\d*$/, message: '请输入数字!' },
+      ];
+    },
+  },
+  {
+    label: '开始日期',
+    field: 'beginDate',
+    component: 'DatePicker',
+    helpMessage: ['指标填报周期起始日期'],
+    componentProps: {
+      disabled: false,
+      allowClear: false,
+      valueFormat: 'YYYY-MM-DD',
+    },
+    rules: [{ required: true, message: '请选择填报指标开始日期!' }],
+  },
+  {
+    label: '结束日期',
+    field: 'endDate',
+    component: 'DatePicker',
+    helpMessage: ['指标填报周期结束日期'],
+    componentProps: {
+      disabled: false,
+      allowClear: false,
+      valueFormat: 'YYYY-MM-DD',
+    },
+    rules: [{ required: true, message: '请选择填报指标结束日期!' }],
+  },
+  {
+    label: '备注',
+    field: 'remark',
+    component: 'Input',
+  },
+  // TODO 主键隐藏字段，目前写死为ID
+  {
+    label: '',
+    field: 'id',
+    component: 'Input',
+    show: false,
+  },
+];
+
+/**
+ * 流程表单调用这个方法获取formSchema
+ * @param param
+ */
+export function getBpmFormSchema(_formData): FormSchema[] {
+  // 默认和原始表单保持一致 如果流程中配置了权限数据，这里需要单独处理formSchema
+  return formSchema;
+}

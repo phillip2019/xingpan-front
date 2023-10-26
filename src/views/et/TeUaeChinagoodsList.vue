374bdf97a7791dcdf7800b1b2acdedb41cf260db
xiaowei.song <songxiaowei@chinagoods.com>
2023-10-26 17:08:19 +0800

    #Updates#埋点验证初步

---
+++ src/views/et/TeUaeChinagoodsList.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <BasicTable
+      :canResize="true"
+      title="生产环境埋点验证"
+      titleHelpMessage="生产环境埋点验证"
+      ref="tableRef"
+      :columns="columns"
+      :dataSource="data"
+      rowKey="id"
+      :rowSelection="{ type: 'checkbox' }"
+    />
+  </div>
+</template>
+<script lang="ts">
+  import { defineComponent, ref, unref } from 'vue';
+  import { BasicTable, TableActionType } from '/@/components/Table';
+  import { columns, searchFormSchema } from './UaeChinagoods.data';
+  import { store } from '/@/store';
+  export default defineComponent({
+    components: { BasicTable },
+    setup() {
+      const tableRef = ref<Nullable<TableActionType>>(null);
+      const data = ref<any[]>([]);
+      const websock = ref<any>();
+
+      function getTableAction() {
+        const tableAction = unref(tableRef);
+        if (!tableAction) {
+          throw new Error('tableAction is null');
+        }
+        return tableAction;
+      }
+      function changeLoading() {
+        getTableAction().setLoading(true);
+        setTimeout(() => {
+          getTableAction().setLoading(false);
+        }, 1000);
+      }
+
+      function initWebSocket() {
+        // WebSocket与普通的请求所用协议有所不同，ws等同于http，wss等同于https
+        var userId = store.getters.userInfo.id;
+        var url = window._CONFIG['domianURL'].replace('https://', 'ws://').replace('http://', 'ws://') + '/websocket/' + userId;
+        websock.value = new WebSocket(url);
+        websock.value.onopen = websocketonopen;
+        websock.value.onerror = websocketonerror;
+        websock.value.onmessage = websocketonmessage;
+        websock.value.onclose = websocketclose;
+      };
+
+      function websocketonopen() {
+        console.log('WebSocket连接成功');
+      }
+
+      function websocketonerror(e) {
+        console.log('WebSocket连接发生错误');
+      }
+      function websocketonmessage(e) {
+        var data = eval('(' + e.data + ')');
+        console.log(`websocket data: ${data}`);
+        //处理订阅信息
+        if (data.cmd == 'topic') {
+          //TODO 系统通知
+          console.log(data);
+        } else if (data.cmd == 'user') {
+          //TODO 用户消息
+          console.log(data);
+        }
+      }
+      function websocketclose(e) {
+        console.log('connection closed (' + e.code + ')');
+      }
+
+      return {
+        tableRef,
+        websock: websock,
+        data: data,
+        columns: columns,
+        formConfig: {
+          labelWidth: 80,
+          schemas: searchFormSchema,
+          autoSubmitOnEnter: true,
+          showAdvancedButton: true,
+          fieldMapToNumber: [],
+          fieldMapToTime: [],
+        },
+        changeLoading,
+      };
+    },
+    onMounted() {},
+    onUnmounted() {},
+  });
+</script>

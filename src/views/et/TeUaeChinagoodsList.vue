ee775514b699c3611ee18ee5589e41ea457a3abd
xiaowei.song <songxiaowei@chinagoods.com>
2023-10-31 16:01:58 +0800

    #Updates#埋点验证完善

--- src/views/et/TeUaeChinagoodsList.vue
+++ src/views/et/TeUaeChinagoodsList.vue
@@ -106,7 +106,7 @@
         // console.log('请求搜索参数为: ')
         // WebSocket与普通的请求所用协议有所不同，ws等同于http，wss等同于https
         // let url = glob.domainUrl?.replace('https://', 'wss://').replace('http://', 'ws://') + '/et/ws/' + userId;
-        let url = glob.domainUrl?.replace('http://', 'ws://') + '/et/ws/' + userId;
+        let url = glob.domainUrl?.replace('https://', 'ws://').replace('http://', 'ws://') + '/et/ws/' + userId;
         url = urlBuilder(url, formValues);
 
         websock.value = new WebSocket(url);

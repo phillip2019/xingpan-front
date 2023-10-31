2625ae1c2958f93607b07c02ff5ee6cb8cfb6ae2
xiaowei.song <songxiaowei@chinagoods.com>
2023-10-31 15:26:36 +0800

    #Updates#埋点验证初步化完成

--- src/views/et/TeUaeChinagoodsList.vue
+++ src/views/et/TeUaeChinagoodsList.vue
@@ -26,6 +26,7 @@
       :bordered="border"
       showTableSetting
       :rowSelection="{ type: 'checkbox' }"
+      :pagination="{ pageSize: 100 }"
     />
   </div>
 </template>
@@ -82,14 +83,31 @@
         }, 1000);
       }
 
-      function initWebSocket() {
+      // 假设有一个urlBuilder组件
+      function urlBuilder(baseURL, params) {
+        // 在组件内部处理参数对象
+        const queryString = Object.keys(params)
+          .filter((key) => params[key] !== undefined)
+          .map((key) => `${key}=${encodeURIComponent(params[key])}`)
+          .join('&');
+
+        // 构建最终的URL链接
+        const finalURL = baseURL + (queryString.length > 0 ? `?${queryString}` : '');
+
+        return finalURL;
+      }
+
+      function initWebSocket(formValues) {
         // WebSocket与普通的请求所用协议有所不同，ws等同于http，wss等同于https
         // var userId = store.getters.userInfo.id;
         let token = getToken();
         let wsClientId = md5(token);
         let userId = unref(userStore.getUserInfo).id + '_' + wsClientId;
+        // console.log('请求搜索参数为: ')
         // WebSocket与普通的请求所用协议有所不同，ws等同于http，wss等同于https
-        let url = glob.domainUrl?.replace('https://', 'wss://').replace('http://', 'ws://') + '/et/ws/' + userId + '?ip=' + '39.187.239.26';
+        let url = glob.domainUrl?.replace('https://', 'wss://').replace('http://', 'ws://') + '/et/ws/' + userId;
+        url = urlBuilder(url, formValues);
+
         websock.value = new WebSocket(url);
         websock.value.onopen = websocketonopen;
         websock.value.onerror = websocketonerror;
@@ -109,7 +127,7 @@
       function websocketonmessage(e) {
         console.log(`websocket data: ${e}`);
         JSON.parse(e.data).forEach((data: any) => {
-          dataList.value.push(data);
+          dataList.value.unshift(data);
         });
         //处理订阅信息
         // if (data.cmd == 'topic') {
@@ -121,7 +139,18 @@
         // }
       }
       function websocketclose(e) {
-        websock.value && websock.value.close();
+        console.log('已定义: ', unref(websock));
+        if (unref(websock)) {
+          // 关闭WebSocket连接并提供关闭代码和关闭原因
+          console.log('触发websocket连接关闭，用户自动断开连接!!!');
+          // 发送自定义的关闭消息
+          const closeMessage = {
+            type: 'close_connection',
+            reason: 'User requested disconnect',
+          };
+          websock.value.send(JSON.stringify(closeMessage));
+          websock.value.close(1000, '用户主动断开连接');
+        }
         console.log('connection closed (' + e + ')');
       }
 
@@ -145,13 +174,22 @@
         changeLoading,
         handleSubmit: (values: any) => {
           if (!isLoadingFlag.value) {
+            // 校验values的值，若values的值为空，则消息提示，必须填写一个参数值
+            const queryParams = Object.keys(values).filter((key) => values[key] !== undefined);
+            if (queryParams.length === 0) {
+              createMessage.error('请求参数必填其一，埋点验证请填写任何一个参数!!!');
+              return;
+            }
+
             submitButtonOptions.value.loading = false;
             submitButtonOptions.value.text = '停止';
             isLoadingFlag.value = true;
             createMessage.success('click search,values:' + JSON.stringify(values));
 
             // 初始化websocket
-            initWebSocket();
+            initWebSocket(values);
+            // 清空
+            dataList.value = [];
             // 打开websocket连接
             // websock.value.onopen();
             return;

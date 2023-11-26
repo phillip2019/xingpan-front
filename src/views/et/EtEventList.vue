91bef3792edc0c13bc18739961e9762a024212a3
stevan <734506700qq@gmail.com>
2023-11-27 00:11:21 +0800

    #Updates#埋点点位完善

--- src/views/et/EtEventList.vue
+++ src/views/et/EtEventList.vue
@@ -87,11 +87,12 @@
         <div style="display: none">
           <ImagePreviewGroup :preview="{ visible, onVisibleChange: (vis) => (visible = vis) }">
             <!--迭代text对象数组，以便循环展示图片-->
-            <Image v-for="(item, index) in text" :key="index" :src="getFileAccessHttpUrl(item?.screenshot)" />
-            <Image v-for="(item, index) in text" :key="index" :src="getFileAccessHttpUrl(item?.screenshot)" />
+            <Image v-for="(item, index) in text" :key="index" :src="getFileAccessHttpUrl(item.screenshot)" />
+            <!-- <Image src="https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp" />
+            <Image src="https://gw.alipayobjects.com/zos/antfincdn/cV16ZqzMjW/photo-1473091540282-9b846e7965e3.webp" />
+            <Image src="https://gw.alipayobjects.com/zos/antfincdn/x43I27A55%26/photo-1438109491414-7198515b166b.webp" /> -->
           </ImagePreviewGroup>
         </div>
-        <!-- <Image :src="getFileAccessHttpUrl(text)" shape="square" :width="40" style="marginright: '1px'" /> -->
       </template>
     </BasicTable>
     <!-- 表单区域 -->

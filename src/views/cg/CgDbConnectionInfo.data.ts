5741047a1ab26fae793b7d98f9f209fc00200aef
xiaowei.song <songxiaowei@chinagoods.com>
2024-10-11 17:26:43 +0800

    #Updates#数据源连接信息更新

--- src/views/cg/CgDbConnectionInfo.data.ts
+++ src/views/cg/CgDbConnectionInfo.data.ts
@@ -164,7 +164,7 @@ export const searchFormSchema: FormSchema[] = [
     componentProps: ({ schema, tableAction, formActionType, formModel }) => {
       let sqlPreTpl = "cg_db_connection_info,sys,sys,1=1 and sys != '' ";
       if (formModel.buName) {
-        sqlPreTpl = sqlPreTpl + " and buName = '" + formModel.buName + "'";
+        sqlPreTpl = sqlPreTpl + " and bu_name = '" + formModel.buName + "'";
       }
       sqlPreTpl += 'group by sys';
       return {
